2d312e1e730a3658b53e30867ae05fca6d3c3ce3
Oleh Lutsenko <oleh.lutsenko@gmail.com>
2024-01-28 09:12:39 +0000

    move game board in separate component

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,6 +3,7 @@
 	import type { CellState, GameState, Player } from '$lib/types';
 	import { checkWin } from '$lib/helper';
 	import GameResultOverlay from './GameResultOverlay.svelte';
+	import GameBoard from './GameBoard.svelte';
 
 	const cells: CellState[] = new Array(9).fill(NO_WINNER);
 	let activePlayer: Player = 0;
@@ -21,17 +22,7 @@
 </script>
 
 <div class="container">
-	<div class="game-container">
-		{#each cells as cell, i}
-			<button
-				class="cell"
-				class:circle={cell === 0}
-				class:cross={cell === 1}
-				on:click={handleTurn(i)}
-			></button>
-		{/each}
-	</div>
-
+	<GameBoard {cells} {handleTurn} />
 	<GameResultOverlay {winner} />
 </div>
 
@@ -43,68 +34,5 @@
 		place-content: center;
 		width: var(--game-size);
 		height: var(--game-size);
-	}
-
-	.game-container {
-		grid-area: 1 / 1 / -1 / -1;
-	}
-
-	.game-container {
-		display: grid;
-		grid-area: 1 / 1 / -1 / -1;
-		background-color: hsl(0, 0%, 20%);
-		grid-template-columns: repeat(3, 1fr);
-		grid-template-rows: repeat(3, 1fr);
-		gap: 1px;
-		min-width: 35px;
-		min-height: 35px;
-	}
-
-	.cell {
-		background-color: hsl(0, 0%, 100%);
-		border: none;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
-
-	.cell.circle, .cell.cross {
-		pointer-events: none;
-	}
-
-	.cell.circle::before,
-	.cell.circle::after {
-		content: '';
-		position: absolute;
-		border-radius: 50%;
-	}
-
-	.cell.circle::before {
-		width: calc(var(--game-size) / 5);
-		height: calc(var(--game-size) / 5);
-		background-color: hsl(0, 0%, 20%);
-	}
-
-	.cell.circle::after {
-		width: calc(var(--game-size) / 8);
-		height: calc(var(--game-size) / 8);
-		background-color: hsl(0, 0%, 100%);
-	}
-
-	.cell.cross::before,
-	.cell.cross::after {
-		content: '';
-		position: absolute;
-		background-color: hsl(0, 0%, 20%);
-		height: calc(var(--game-size) / 5);
-		width: calc(var(--game-size) / 25);
-	}
-
-	.cell.cross::before {
-		transform: rotate(45deg);
-	}
-
-	.cell.cross::after {
-		transform: rotate(-45deg);
 	}
 </style>
---
+++ src/routes/GameBoard.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+	import type { CellState } from '$lib/types';
+
+	export let cells: CellState[];
+	export let handleTurn: (cellIndex: number) => (e: MouseEvent) => void;
+</script>
+
+<div class="game-container">
+	{#each cells as cell, i}
+		<button
+			class="cell"
+			class:circle={cell === 0}
+			class:cross={cell === 1}
+			on:click={handleTurn(i)}
+		/>
+	{/each}
+</div>
+
+<style>
+	.game-container {
+		display: grid;
+		grid-area: 1 / 1 / -1 / -1;
+		background-color: hsl(0, 0%, 20%);
+		grid-template-columns: repeat(3, 1fr);
+		grid-template-rows: repeat(3, 1fr);
+		gap: 1px;
+		min-width: 35px;
+		min-height: 35px;
+	}
+
+	.cell {
+		background-color: hsl(0, 0%, 100%);
+		border: none;
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+
+	.cell.circle,
+	.cell.cross {
+		pointer-events: none;
+	}
+
+	.cell.circle::before,
+	.cell.circle::after {
+		content: '';
+		position: absolute;
+		border-radius: 50%;
+	}
+
+	.cell.circle::before {
+		width: calc(var(--game-size) / 5);
+		height: calc(var(--game-size) / 5);
+		background-color: hsl(0, 0%, 20%);
+	}
+
+	.cell.circle::after {
+		width: calc(var(--game-size) / 8);
+		height: calc(var(--game-size) / 8);
+		background-color: hsl(0, 0%, 100%);
+	}
+
+	.cell.cross::before,
+	.cell.cross::after {
+		content: '';
+		position: absolute;
+		background-color: hsl(0, 0%, 20%);
+		height: calc(var(--game-size) / 5);
+		width: calc(var(--game-size) / 25);
+	}
+
+	.cell.cross::before {
+		transform: rotate(45deg);
+	}
+
+	.cell.cross::after {
+		transform: rotate(-45deg);
+	}
+</style>
