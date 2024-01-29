6d4827fbec3c0248efd675d86baf17191996b279
Oleh Lutsenko <oleh.lutsenko@gmail.com>
2024-01-29 10:48:18 +0000

    change winner overlay

--- src/routes/GameResultOverlay.svelte
+++ src/routes/GameResultOverlay.svelte
@@ -7,11 +7,11 @@
 {#if winner > NO_WINNER}
 	<div class="game-result-overlay">
 		{#if winner === 0}
-			<h1>Player 1 wins!</h1>
+			<h1>O wins!</h1>
 		{:else if winner === 1}
-			<h1>Player 2 wins!</h1>
+			<h1>X wins!</h1>
 		{:else if winner === 2}
-			<h1>It's a draw!</h1>
+			<h1>Draw!</h1>
 		{/if}
 	</div>
 {/if}
@@ -21,8 +21,8 @@
 		grid-area: 1 / 1 / -1 / -1;
 		display: grid;
 		place-content: center;
-		background-color: hsla(0, 0%, 0%, 0.9);
-		color: hsla(0, 0%, 100%, 1);
+		background-color: hsla(0, 0%, 0%, 0.7);
+		color: hsla(0, 0%, 100%, 1);		
 		z-index: 1;
 	}
 </style>
