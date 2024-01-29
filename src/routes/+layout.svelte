add31b6196f52f745b2ce524bf897d787d7b4643
Oleh Lutsenko <oleh.lutsenko@gmail.com>
2024-01-29 10:42:41 +0000

    change background color

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -11,6 +11,7 @@
 		--game-size: 90vmin;
 		--blue: hsl(253, 87%, 79%);
 		--darkblue: hsl(253, 76%, 62%);
+		--off-white: hsl(35, 51%, 93%);
 	}
 
 	@media (min-width: 601px) {
@@ -24,5 +25,6 @@
 		min-height: 100vh;
 		display: grid;
 		place-content: center;
+		background-color: var(--off-white);
 	}
 </style>
